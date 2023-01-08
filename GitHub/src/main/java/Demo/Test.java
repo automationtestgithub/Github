449a002a5943c47b60d56554835de4a530a1aadf
nikhi <nikhi@Nikhil>
2023-01-08 19:18:15 +0530

    My first project.

---
+++ GitHub/src/main/java/Demo/Test.java
@@ -0,0 +1,13 @@
+package Demo;
+public class Test{
+	public static void main (String args) 
+	{
+	String Hungry ="Yes";
+	if (Hungry == "Yes")
+		System.out.println("Eat something...");
+	else
+	{
+		System.out.println("Do work...");
+	}
+	}
+}
